f17bd79e91e2672d283eee595ae9ee8221f26d57
JavaKira <JavaKira@yandex.ru>
2023-04-26 09:47:56 +0300

    Feat: adds viewBindings and MainActivity

--- app/src/main/java/com/github/javakira/whattoeat/MainActivity.java
+++ app/src/main/java/com/github/javakira/whattoeat/MainActivity.java
@@ -3,12 +3,35 @@ package com.github.javakira.whattoeat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.javakira.whattoeat.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.switch1.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b)
+                setDishes();
+            else
+                setProducts();
+        });
+    }
+
+    private void setProducts() {
+        binding.dishes.setVisibility(View.GONE);
+        binding.products.setVisibility(View.VISIBLE);
+    }
+
+    private void setDishes() {
+        binding.dishes.setVisibility(View.VISIBLE);
+        binding.products.setVisibility(View.GONE);
     }
 }
